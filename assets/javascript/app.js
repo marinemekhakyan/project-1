8dd05af728476a3859a6f55de69d98791d32e469
Kristina Calumpong <kcalumpong@gmail.com>
2019-08-02 21:50:01 -0700

    updated files

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+
+var ingredients = "";
+var cuisine = "";
+$(document).ready(function () {
+    $(".btn").on("click", function (event) {
+        event.preventDefault();
+        $("#main-container").hide();
+
+        cuisine = $("#cuisine-input").val().trim();
+        ingredients = $("#user-ingredients").val().trim();
+
+        var queryURL = ("https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=" + ingredients + "&q=" + cuisine + "&p=1");
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+
+    });
+})
